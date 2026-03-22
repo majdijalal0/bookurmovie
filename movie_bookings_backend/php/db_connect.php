1281d444c97cec7c9d677c958ea0ed7b633c7eee
majdijalal0 <majdijalal0@gmail.com>
2026-03-22 11:54:46 +0100

    update db config for Railway environment

--- movie_bookings_backend/php/db_connect.php
+++ movie_bookings_backend/php/db_connect.php
@@ -2,7 +2,11 @@
 require_once __DIR__ . '/config.php';
 
 try {
-    $conn = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
+    $conn = new PDO(
+        "mysql:host=" . DB_SERVER . ";port=" . DB_PORT . ";dbname=" . DB_NAME . ";charset=utf8",
+        DB_USERNAME,
+        DB_PASSWORD
+    );
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     http_response_code(500);
